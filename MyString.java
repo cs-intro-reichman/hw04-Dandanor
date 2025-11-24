a2c9437f2bf6dcb885feac544ffe2b92ca34a667
Dandanor <Dandanor2003@gmail.com>
2025-11-24 21:11:37 +0200

    completed mystring

--- MyString.java
+++ MyString.java
@@ -18,13 +18,37 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newstr="";
+        for (int i=0;i<str.length();i++){
+            if(str.charAt(i)>=65&&str.charAt(i)<=90){
+            newstr+=(char)(str.charAt(i)+32);
+            }
+            else{
+                newstr+=str.charAt(i);
+            }
+        }
+        return newstr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        boolean check=true;
+        if ((str1.indexOf(str2.charAt(0))==-1)){
+            return false;
+        }
+        else{
+            char firstc=str2.charAt(0);
+            for (int i=0; i<str1.length();i++){
+                if(str1.charAt(i)==firstc){
+                    check=true;
+                    for (int j=0;j<str2.length();j++){
+                        if(str1.charAt(i+j)!=str2.charAt(j)){
+                            check = false;
+                        }
+                    }
+                }
+            }
+            return check;
+        }
     }
 }
